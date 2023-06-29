3fb1cdef38dc5b964911ef7638a818f1f69fa41b
Noor Ul Islam <noorulislam770@gmail.com>
2023-06-29 14:23:57 +0500

    Angular Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector :'app-root',
+  templateUrl:'./app.component.html',
+  styleUrls:['./app.component.css']
 })
-export class AppComponent {
-  title = 'basics';
+export class AppComponent{
+  name = 'noor'
+  imgUrl = 'https://picsum.photos/id/237/500/500'
+  getName(){
+    return this.name.toUpperCase()
+  }
+
+  changeImage(e:KeyboardEvent){
+    this.imgUrl = (e.target as HTMLInputElement).value;
+  }
+
+
 }
