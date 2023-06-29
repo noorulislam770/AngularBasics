2e5eeefb9b78ccd595fdaaf43b8a23f4fef6c775
Noor Ul Islam <noorulislam770@gmail.com>
2023-06-29 22:28:09 +0500

    components fundementals and transforming content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,35 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
-  selector :'app-root',
-  templateUrl:'./app.component.html',
-  styleUrls:['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
-  name = 'noor'
-  imgUrl = 'https://picsum.photos/id/237/500/500'
-  getName(){
-    return this.name.toUpperCase()
+export class AppComponent {
+  name = 'noor ul islam';
+  currentDate = new Date();
+  pizza = {
+    toppings: ['pepperoni', 'beacon'],
+    size: 'large',
+  };
+  cost = 2000;
+  blueClass = false;
+  fontSize = 20;
+  temperature = 25.3;
+  imgUrl = 'https://picsum.photos/id/237/500/500';
+  images = [
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/236/500/500',
+    'https://picsum.photos/id/235/500/500',
+  ];
+  getName() {
+    return this.name.toUpperCase();
   }
 
-  changeImage(e:KeyboardEvent){
+  changeImage(e: KeyboardEvent) {
     this.imgUrl = (e.target as HTMLInputElement).value;
   }
 
-  logImg(event:string){
-    console.log(event)
+  logImg(event: string) {
+    console.log(event);
   }
 }
