319e71387dcad9ca2892917ea3699dc6470951b8
Noor Ul Islam <noorulislam770@gmail.com>
2023-06-29 15:52:05 +0500

    components section done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,5 +15,7 @@ export class AppComponent{
     this.imgUrl = (e.target as HTMLInputElement).value;
   }
 
-
+  logImg(event:string){
+    console.log(event)
+  }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,46 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,EventEmitter, Output ,
+  OnInit,OnChanges,DoCheck,AfterContentChecked,
+  AfterContentInit,AfterViewChecked,AfterViewInit,
+  OnDestroy
+  } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
-  @Input('img') postImg = '';
+export class PostComponent implements OnDestroy,OnInit,OnChanges,DoCheck,AfterContentChecked,AfterContentInit,AfterViewChecked,AfterViewInit{
 
+  constructor() {
+    console.log('constructor called')
+  }
+
+  @Input('img') postImg = '';
+  @Output()imgSelected = new EventEmitter<string>();
+
+  ngOnInit(){
+    console.log('ngoninit() called',this.postImg)
+  }
+
+  ngOnChanges(){
+    console.log("on changes called")
+  }
+  ngDoCheck(){
+    console.log("docheck() called")
+  }
+  ngAfterContentChecked(){
+    console.log("after content checked")
+  }
+  ngAfterContentInit(){
+    console.log("after content init")
+  }
+  ngAfterViewChecked(){
+    console.log("after view checked")
+  }
+  ngAfterViewInit(){
+    console.log("after view init")
+  }
+  ngOnDestroy(): void {
+    console.log('ngondestroy() called')
+  }
 }
